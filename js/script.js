c6d59c604be1efac0f4ec68f6a8ff28b45372e5a
Aveskulov <WenJoymk3@gmail.com>
2023-08-14 20:59:53 +0200

    calendar mounth

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 window.onload = () => {
 	window.setInterval(() => {
 		const date = new Date();
-		const hours = SetTimeFormat(date.getHours());
-		const minutes = SetTimeFormat(date.getMinutes());
-		const seconds = SetTimeFormat(date.getSeconds());
+		const hours = MySetTimeFormat(date.getHours());
+		const minutes = MySetTimeFormat(date.getMinutes());
+		const seconds = MySetTimeFormat(date.getSeconds());
 		const clock = hours + ":" + minutes + ":" + seconds;
 		const calendarTime = document.querySelector(".clock");
 		calendarTime.innerHTML = clock;
@@ -12,24 +12,40 @@ window.onload = () => {
 		if (hours < 12) { PmAm.innerHTML = "AM"; } else { PmAm.innerHTML = "PM"; }
 
 		const dayToday = document.querySelector(".time__data");
-		const day = `${SetDaysOfTheWeek(date.getDay())}, ${SetMonth(date.getMonth())} ${date.getDate()} ${date.getFullYear()}`;
+		const day = `${MySetDaysOfTheWeek(date.getDay())}, ${MySetMonth(date.getMonth())} ${date.getDate()} ${date.getFullYear()}`;
 		console.log(day);
 		dayToday.innerHTML = day;
 
 	}, 1000)
 }
 
-function SetTimeFormat(date) {
+const date = new Date();
+const calendarMonthYear = document.querySelector(".calendar__month");
+const monthYear = `${MySetMonth(date.getMonth())} ${date.getFullYear()}`;
+calendarMonthYear.innerHTML = monthYear;
+
+const buttonUp = document.querySelector(".up");
+const buttonDown = document.querySelector(".down");
+buttonUp.addEventListener("click", () => {
+	date.setMonth((date.getMonth()) - 1)
+	calendarMonthYear.innerHTML = `${MySetMonth(date.getMonth())} ${date.getFullYear()}`;
+})
+buttonDown.addEventListener("click", () => {
+	date.setMonth((date.getMonth()) + 1)
+	calendarMonthYear.innerHTML = `${MySetMonth(date.getMonth())} ${date.getFullYear()}`;
+})
+
+function MySetTimeFormat(date) {
 	let time = date;
 	return (time < 10) ? time = "0" + time : time;
 }
 
-function SetDaysOfTheWeek(date) {
+function MySetDaysOfTheWeek(date) {
 	const daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 	return (daysOfTheWeek[date - 1]);
 }
 
-function SetMonth(date) {
+function MySetMonth(date) {
 	const masMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	return (masMonth[date]);
 }
